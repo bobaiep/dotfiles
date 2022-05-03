0382d26db8ebe79acdf711916055c3414c5489b5
Marius ANDRE <marius.andre@epita.fr>
2022-05-03 11:51:31 +0200

    Small improvement on polybar launch script

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,8 +5,14 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar -c ~/.config/polybar/config  main -r >>/tmp/polybar1.log 2>&1 & disown
-polybar -c ~/.config/polybar/config main2 -r >>/tmp/polybar1.log 2>&1 & disown
+
+if hostname -s | grep -q "boba-xps13"; then
+    # Launch bar1 and bar2
+    polybar -c ~/.config/polybar/config main-laptop -r >>/tmp/polybar1.log 2>&1 & disown
+else
+    polybar -c ~/.config/polybar/config  main -r >>/tmp/polybar1.log 2>&1 & disown
+    polybar -c ~/.config/polybar/config main2 -r >>/tmp/polybar2.log 2>&1 & disown
+fi
+
 echo "Bars launched..."
